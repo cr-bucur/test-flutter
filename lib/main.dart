b852a016f2e804e3d11d0ea717522f137fec4a0e
Your Name <you@example.com>
2021-04-14 18:09:07 +0300

    modificare cu fisier nou

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:-----_-----_-_',
+              'You have pushed the button this many times:--_---_-----_-_',
             ),
             Text(
               '$_counter',
